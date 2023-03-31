087737dee621ba71c6ac18fec028d906072d2137
lsy3709 <lsy3709@naver.com>
2023-03-31 17:41:23 +0900

    ch5 저장4

--- ch5_test/Self05-03.py
+++ ch5_test/Self05-03.py
@@ -1,17 +1,26 @@
 inFp = None	# 입력 파일
+inList = []
 inStr = ""		# 읽어온 문자열
 lineNum = 1
 
-inFp = open("test1.txt","r")
+with open("test1.txt","r") as inFp :
+# inFp = open("test1.txt","r")
 # inFp = open("C:/Temp/data1.txt", "r", encoding="utf-8" )
 #inFp = open("C:/Temp/data1.txt", "r")
 
-while True :
-    inStr = inFp.readline()
-    if inStr == "" :
-        print("")
-        break;
-    print("%d : %s" %(lineNum, inStr), end="")
-    lineNum += 1
+# while True :
+#     inStr = inFp.readline()
+#     if inStr == "" :
+#         print("")
+#         break;
+#     print("%d : %s" %(lineNum, inStr), end="")
+#     lineNum += 1
 
-inFp.close()
+    inList = inFp.readlines()
+print(inList)
+
+for inStr in inList:
+   print("%d : %s" % (lineNum,inStr), end="")
+   lineNum += 1
+
+# inFp.close()
