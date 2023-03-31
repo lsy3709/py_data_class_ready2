295040d83e6b2f5ae0626c90c192b1a59642cd3a
lsy3709 <lsy3709@naver.com>
2023-03-31 14:26:58 +0900

    ch5 저장2

---
+++ ch5_test/test1.py
@@ -0,0 +1,169 @@
+
+# 기본 창
+
+# from tkinter import *
+
+# window = Tk() 
+
+# # 옵션 값 추가 
+# window.title("윈도우창 연습")
+# window.geometry("400x100")
+# window.resizable(width = TRUE, height = TRUE)
+
+# window.mainloop()
+
+
+
+# 텍스트 뷰 라벨
+
+# from tkinter import * 
+# window = Tk() 
+
+# window.title("윈도우창 연습")
+# window.geometry("400x100")
+# window.resizable(width = TRUE, height = TRUE)
+# label1 = Label(window, text = "COOKBOOK, 데이터 분석을")
+# label2 = Label(window, text = "열심히", font=("궁서체",30),fg="blue", bg="white")
+# label3 = Label(window, text = "공부 중입니다.", bg="magenta", \
+#   width=20, height=5, anchor=NW)
+
+# label1.pack()
+# label2.pack()
+# label3.pack()
+
+# window.mainloop()
+
+
+
+#이미지
+
+# from tkinter import *
+
+# window = Tk()
+
+# # .jpg 못 읽음. 
+# photo1 = PhotoImage(file="sample_img/childLook.gif")
+# photo2 = PhotoImage(file="sample_img/라바1.png")
+# # photo3 = PhotoImage(file="sample_img/cherries.jpg")
+# label1 = Label(window, image=photo1)
+# label2 = Label(window, image=photo2)
+# # label3 = Label(window, image=photo3)
+
+# label1.pack(side=LEFT)
+# label2.pack()
+# # label3.pack()
+
+# window.mainloop()
+
+
+# 버튼
+
+# from tkinter import * 
+# window = Tk()
+
+# button1 = Button(window, text="종료", fg="red", command=quit)
+
+# button1.pack()
+
+# window.mainloop()
+
+
+
+# 버튼 이미지 ,
+
+# from tkinter import * 
+# from tkinter import messagebox
+
+
+# def myFunc() :
+#   messagebox.showinfo("창 잘 뜨나요?","잘 뜨는 군")
+  
+# window = Tk()
+
+# photo = PhotoImage(file="sample_img/라바1.png")
+# button1 = Button(window, image=photo, command=myFunc)
+
+# button1.pack()
+
+# window.mainloop()
+
+
+# from tkinter import * 
+# from tkinter import messagebox
+
+# window = Tk()
+
+# def myFunc() :
+#   if chk.get() == 0:
+#     messagebox.showinfo("","체크버튼이 꺼졌어요: "+str(chk.get()))
+#   else :
+#     messagebox.showinfo("","체크버튼이 켜졌어요: "+ str(chk.get()))
+
+# chk = IntVar()
+# cb1 = Checkbutton(window,text = "클릭하세요",variable = chk, 
+#                   command =myFunc)
+
+# cb1.pack()
+
+# window.mainloop()
+  
+# from tkinter import *
+# from tkinter import messagebox
+# window = Tk()
+# window.geometry("400x100")
+# window.resizable(width = TRUE, height = TRUE)
+# photo2 = PhotoImage(file="sample_img/라바1.png")
+
+# def myFunc() :
+#   if var.get() == 1:
+#     label1.configure(text = "파이썬")
+#   elif var.get() == 2:
+#     label1.configure(text = "C++")
+#   else :
+#     label1.configure(text="Java")
+
+# var = IntVar()
+# rb1 = Radiobutton(window, text="파이썬",
+#                   variable=var, value=1,command=myFunc)
+# rb2 = Radiobutton(window, text="C++",
+#                   variable=var, value=2,command=myFunc)
+# rb3 = Radiobutton(window, text="Java",
+#                   variable=var, value=3,command=myFunc)
+
+    
+# label1 = Label(window,text="선택한 언어 : ", fg ="red")
+
+# rb1.pack()
+# rb2.pack()
+# rb3.pack()
+# label1.pack()
+
+# window.mainloop()
+
+# from tkinter import *
+# window = Tk()
+
+# button1 = Button(window, text="버튼1")
+# button2 = Button(window, text="버튼2")
+# button3 = Button(window, text="버튼3")
+
+# button1.pack(side = LEFT)
+# button2.pack(side = LEFT)
+# button3.pack(side = LEFT)
+
+# window.mainloop()
+
+
+from tkinter import * 
+window = Tk() 
+
+btnList = [None] * 3 
+
+for i in range(0,3) :
+  btnList[i] = Button(window, text="버튼" + str(i+1))
+  
+for btn in btnList :
+  btn.pack(side = TOP,fill = X, ipadx = 10, ipady = 10)
+  
+window.mainloop() 
+
