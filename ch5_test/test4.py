e1edc89ceb3aa1bfe2ca1be5b2bf0f85befe6123
lsy3709 <lsy3709@naver.com>
2023-04-03 09:58:12 +0900

    ch5 확인 5

--- ch5_test/test4.py
+++ ch5_test/test4.py
@@ -1,14 +1,30 @@
-outFp = None
-outStr=""
+# outFp = None
+# outStr=""
 
-outFp = open("test2.txt","w")
+# outFp = open("test2.txt","w")
 
-while True:
-  outStr = input("내용 입력: ")
-  if outStr != "":
-    outFp.writelines(outStr + "\n")
-  else:
-    break
+# while True:
+#   outStr = input("내용 입력: ")
+#   if outStr != "":
+#     outFp.writelines(outStr + "\n")
+#   else:
+#     break
 
+# outFp.close()
+# print("정상 파일 씀.")
+
+
+#윈도우즈로 확인하기.
+inFp, outFp = None, None 
+inStr = ""
+
+inFp = open("C:/Windows/win.ini", "r")
+outFp = open("C:/Temp/data3.txt", "w")
+
+inList = inFp.readlines()
+for inStr in inList :
+    outFp.writelines(inStr)
+
+inFp.close()
 outFp.close()
-print("정상 파일 씀.")
+print("--- 파일이 정상적으로 복사되었음 ---")
